c4e1811ed7d0a3bb144cb78ab31fa45f469b74de
Frederick Boehm <frederick.boehm@gmail.com>
2017-12-29 18:19:00 -0600

    created submit files for many runs

--- shell_scripts/pvl400.sh
+++ shell_scripts/pvl400.sh
@@ -7,5 +7,5 @@ tar -xzf SLIBS.tar.gz
 export PATH=$(pwd)/R/bin:$PATH
 export LD_LIBRARY_PATH=$(pwd)/SS:$LD_LIBRARY_PATH
 # run R, with the name of your  R script
-R CMD BATCH '--args argname='$1' nsnp='$2' s1='$3' run_num='$4'' Rscript/pvl400.R ''$1'_pvl400_run'$4'.Rout'
+R CMD BATCH '--args argname='$1' nsnp='$2' s1='$3' run_num='$4'' Rscript/pvl400.R 'pvl400_run'$4'-'$1'.Rout'
 
