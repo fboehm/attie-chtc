b640dd676cadc6c869c14ce228dc438854fec902
Frederick Boehm <fjboehm@wisc.edu>
2018-02-17 12:24:34 -0600

    switched to using R7.tar.gz

--- shell_scripts/pvl-chr17.sh
+++ shell_scripts/pvl-chr17.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # untar your R installation
-tar -xzf R6.tar.gz
+tar -xzf R7.tar.gz
 tar -xzf SLIBS.tar.gz
 # make sure the script will use your R installation
 export PATH=$(pwd)/R/bin:$PATH
